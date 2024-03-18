be86d093faf687e86c790f09b263696facdc3aff
Hà Âu Nhật Minh <112295441+Ryugwki@users.noreply.github.com>
2024-03-18 14:33:50 +0700

    Add files via upload

---
+++ ResponsiveCoffeeShop/js/script.js
@@ -0,0 +1,27 @@
+const navbar = document.querySelector('.navbar');
+const cartItem = document.querySelector('.cart-items-container');
+const searchForm = document.querySelector('.search-form');
+
+
+document.querySelector('#menu-btn').onclick = () => {
+       navbar.classList.toggle('active');
+       cartItem.classList.remove('active');
+       searchForm.classList.remove('active');
+}
+
+document.querySelector('#cart-btn').onclick = () => {
+       cartItem.classList.toggle('active');
+       navbar.classList.remove('active');
+       searchForm.classList.remove('active');
+}
+
+document.querySelector('#search-btn').onclick = () => {
+       searchForm.classList.toggle('active');
+       navbar.classList.remove('active');
+       cartItem.classList.remove('active');
+}
+window.onscroll = () => {
+       navbar.classList.remove('active');
+       cartItem.classList.remove('active');
+       searchForm.classList.remove('active');
+}
